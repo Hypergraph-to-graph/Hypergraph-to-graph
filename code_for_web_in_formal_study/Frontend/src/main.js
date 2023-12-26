4d73a56eca23b0cfcdf1ad9e6cd2e630a4927b16
穆瑞新 <3274333659@qq.com>
2023-12-26 15:15:10 +0800

    modify in 12.26

--- code_for_web_in_formal_study/Frontend/src/main.js
+++ code_for_web_in_formal_study/Frontend/src/main.js
@@ -5,6 +5,6 @@ import Vuex from 'vuex';
 import store from '@/store/index.js'
 import 'element-plus/theme-chalk/index.css';
 import locale from 'element-plus/lib/locale/lang/zh-cn'
-import 'default-passive-events'
+import 'default-passive-events' 
 
 createApp(App).use(scroll).use(Vuex).use(store).use(ElementPlus, { locale }).mount("#app");
